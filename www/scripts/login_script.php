a1bd3121f64c586228f603cefe6a3d9521f6bb1c
mtauber <mmtauber@gmail.com>
2012-06-15 08:35:45 -0700

    Security

--- www/scripts/login_script.php
+++ www/scripts/login_script.php
@@ -3,8 +3,8 @@
 $user = $_POST['user'];
 $password = $_POST['password'];
 
-if ($user == "tallbikejoust") {
-	if ($password ==  "packback102938") {	
+if ($user == "test") {
+	if ($password ==  "test") {	
 		session_register("tallbikejoust");
 		session_register("packback102938");
 		header("location:/admin_index.php");
