6d0dc3b46f969f0316708f89c5f2adb687f41d8f
mtauber <mmtauber@gmail.com>
2012-10-28 13:26:13 -0700

    updated cred logic

--- www/scripts/login_script.php
+++ www/scripts/login_script.php
@@ -1,11 +1,12 @@
 <?php
 
-$user = $_POST['user'];
-$password = $_POST['password'];
+include('../cred.php');
 
-if ($user == "test") {
-	if ($password ==  "test") {	
-		session_register("");
+$user = $_POST['user'];
+$pass = $_POST['password'];
+
+if( crypt($user, $username) == $username) {
+	if( crypt($pass, $password) == $password) {	
 		session_register("");
 		header("location:/admin_index.php");
 	}
